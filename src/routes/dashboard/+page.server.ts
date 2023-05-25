e4a023e71a2d852ae95ea9d2ed9222c046d91ef0
Nintron <nintron@sent.com>
2023-05-25 01:19:15 -0400

    just show the cookie

--- src/routes/dashboard/+page.server.ts
+++ src/routes/dashboard/+page.server.ts
@@ -3,59 +3,60 @@ import type { PageServerLoad } from "./$types";
 import { parse, parseNumberAndBigInt } from "lossless-json";
 import { z } from "zod";
 
-export const load = (async ({ fetch }) => {
-	const Assets = z.array(
-		z.object({
-			asset: z.object({
-				id: z.coerce.bigint(),
-				name: z.string(),
-				value: z.coerce.bigint()
-			}),
-			quantity: z.coerce.number()
-		})
-	);
+export const load = (async ({ fetch, cookies }) => {
+	// const Assets = z.array(
+	// 	z.object({
+	// 		asset: z.object({
+	// 			id: z.coerce.bigint(),
+	// 			name: z.string(),
+	// 			value: z.coerce.bigint()
+	// 		}),
+	// 		quantity: z.coerce.number()
+	// 	})
+	// );
 
-	let assetError: string | undefined;
+	// let assetError: string | undefined;
 
-	let assetResponse = await fetch(`${PUBLIC_STL_API}/wallet/assets`, {
-		method: "GET",
-		credentials: "include"
-	});
+	// let assetResponse = await fetch(`${PUBLIC_STL_API}/wallet/assets`, {
+	// 	method: "GET",
+	// 	credentials: "include"
+	// });
 
-	if (assetResponse.status >= 400) {
-		assetError = await assetResponse.text();
-	}
+	// if (assetResponse.status >= 400) {
+	// 	assetError = await assetResponse.text();
+	// }
 
-	const Transactions = z.array(
-		z.object({
-			id: z.coerce.bigint(),
-			sending_wallet_id: z.coerce.bigint(),
-			receiving_wallet_id: z.coerce.bigint(),
-			created_at: z.string(),
-			memo: z.string(),
-			assets: Assets
-		})
-	);
+	// const Transactions = z.array(
+	// 	z.object({
+	// 		id: z.coerce.bigint(),
+	// 		sending_wallet_id: z.coerce.bigint(),
+	// 		receiving_wallet_id: z.coerce.bigint(),
+	// 		created_at: z.string(),
+	// 		memo: z.string(),
+	// 		assets: Assets
+	// 	})
+	// );
 
-	let txError: string | undefined;
+	// let txError: string | undefined;
 
-	let txResponse = await fetch(`${PUBLIC_STL_API}/wallet/transactions`, {
-		method: "GET",
-		credentials: "include"
-	});
+	// let txResponse = await fetch(`${PUBLIC_STL_API}/wallet/transactions`, {
+	// 	method: "GET",
+	// 	credentials: "include"
+	// });
 
-	if (txResponse.status >= 400) {
-		txError = await txResponse.text();
-	}
+	// if (txResponse.status >= 400) {
+	// 	txError = await txResponse.text();
+	// }
 
 	return {
-		assetError,
-		assets: assetError
-			? undefined
-			: Assets.safeParse(parse(await assetResponse.text(), undefined, parseNumberAndBigInt)),
-		txError,
-		transactions: txError
-			? undefined
-			: Transactions.safeParse(parse(await txResponse.text(), undefined, parseNumberAndBigInt))
+		// assetError,
+		// assets: assetError
+		// 	? undefined
+		// 	: Assets.safeParse(parse(await assetResponse.text(), undefined, parseNumberAndBigInt)),
+		// txError,
+		// transactions: txError
+		// 	? undefined
+		// 	: Transactions.safeParse(parse(await txResponse.text(), undefined, parseNumberAndBigInt))
+		stlCookie: cookies.get("stelo_token")
 	};
 }) satisfies PageServerLoad;
