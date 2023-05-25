9accec8936766a9a52c46c091af21b020d96e3c8
Nintron <nintron@sent.com>
2023-05-25 02:34:36 -0400

    Change homepage messaging

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,6 @@
 <h1 class="text-2xl">Homepage</h1>
 
-<p class="my-2">If you logged in already, go to dashboard, you'll stay logged in for a month</p>
+<p class="my-2">Login or register, you'll be redirected once logged in</p>
 
 <a href="/login" class="bg-gray-300 rounded-sm py-1 px-4 underline">Login</a>
 <a href="/register" class="bg-gray-300 rounded-sm py-1 px-4 underline">Register</a>
-<a href="/dashboard" class="bg-gray-300 rounded-sm py-1 px-4 underline">Dashboard</a>
